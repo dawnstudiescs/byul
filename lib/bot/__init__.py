2267ac70a6f08d24e04d34563e231fdc70fd568d
dawnstudiescs <khtatbadoha@gmail.com>
2021-01-27 14:44:37 -0800

    embeds and attachments

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,4 +1,8 @@
+from discord import Intents
+from datetime import datetime
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord import Embed
 from discord.ext.commands import Bot as BotBase
 
 PREFIX = "b-"
@@ -12,7 +16,11 @@ class Bot(BotBase):
         self.guild = None
         self.scheduler = AsyncIOScheduler()
 
-        super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
+        super().__init__(
+            command_prefix=PREFIX, 
+            owner_ids=OWNER_IDS,
+            intents=Intents.all()
+        )
 
     def run(self, version):
         self.VERSION = version
@@ -32,8 +40,23 @@ class Bot(BotBase):
     async def on_ready(self):
         if not self.ready:
             self.ready = True
-            #self.guild = self.get_guild(704165535226527765)
+            self.guild = self.get_guild(693443636406386760)
             print("byul ready")
+
+            channel = self.get_channel(697769479710703777)
+            #await channel.send("**Byul** is online.")
+
+            embed = Embed(title="Now online!!", description = "**Byul** is now online.", 
+                            colour=0xC2EABD, timestamp=datetime.utcnow())
+            fields = [("***Mood***", "*Sleepy zzz*", True),
+                        ("***Favourite parent***", "***Dawnu***", True)]
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+            embed.set_thumbnail(url=self.guild.icon_url)
+            embed.set_image(url=self.guild.icon_url)
+            embed.set_footer(text="Take a lollipop!")
+            embed.set_author(name="Dawnu", icon_url=self.guild.icon_url)
+            await channel.send(embed=embed)
 
         else:
             print("byul reconnected")
